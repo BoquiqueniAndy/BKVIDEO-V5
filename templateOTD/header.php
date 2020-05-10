18165fc98d2c1aa48bd08a9c35781210cd973806
BoquiqueniAndy <boquiqueni.andy@gmail.com>
2020-05-10 18:05:21 +0400

    Le site

---
+++ templateOTD/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		
+    </head>
+ 
+    <body>
+        <a role="button" class="navbar-burger" data-target="menu" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+        <aside class="navbar-menu" id="menu">
+            <nav>  
+                <ul>
+                    <li class="title is-6"><a class="is-active" href="index.php?action=Accueil">Accueil</a> </li>
+                    <li class="title is-6"><a class="is-active" href="index.php?action=Inscription">Inscription</a> </li>
+                    <li class="title is-6"><a class="is-active" href="index.php?action=Co">Connexion</a> </li>
+                    <li class="title is-6"><a class="is-active" href="index.php?action=Deco"> Déconnexion </a></li>
+                    <li class="title is-6"><a class="is-active" href="index.php?action=produits">Produits</a> </li>
+                    <li class="title is-6"><a class="is-active" href="index.php?action=panier">Panier</a> </li>
+                    <li class="title is-6"><a class="is-active" href="index.php?action=admin">Espace administrateur</a> </li>
+            
+                </ul>
+            </nav>
+        </aside>
+    </body>
+
+
+</html>
